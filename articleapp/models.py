631e04f964dd01491ab354cfd583f41a87237362
dooli1971039 <dooli9819@gmail.com>
2022-04-10 01:50:54 +0900

    35강 내용

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    #SET_NULL : 나중에 유저가 사라지면, 알수 없음 정도로 표시
+    writer=models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+
+    title = models.CharField(max_length=200,null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    created_at = models.DateField(auto_now_add=True, null=True)
